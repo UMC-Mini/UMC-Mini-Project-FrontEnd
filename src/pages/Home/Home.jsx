5a2b7a5cd99bae25062a8eeee01ab5336cf681a0
funfun <alpha5179@naver.com>
2024-06-24 14:48:21 +0900

    feat: 베너 로그인 버튼  추가

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,9 +7,15 @@ import star from '../../assets/Star.png'
 import star2 from '../../assets/Star2.png'
 import mypage from '../../assets/mypage.png'
 import Chatbot from '../Chatting/Chatbot';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
   const [chatbotVisible, setChatbotVisible] = useState(false);
+  const navigate = useNavigate();
+  const isLogin = useSelector((state) => state.user.isLogin);
+  const userName = localStorage.getItem("userName");
+  const userNickname = localStorage.getItem("userNickname");
 
   const handleChatbotToggle = () => {
     setChatbotVisible(!chatbotVisible);
@@ -25,10 +31,17 @@ const Home = () => {
         <img src={handImage}/>
       </S.Left>
       <S.Right>
-        <S.Welcome><img src={mypage}/><p>24학번 김슴우님 스뮤풀에 오신 것을 환영합니다!</p></S.Welcome>
+      <S.Welcome>
+          <img src={mypage}/>
+          {isLogin ? (
+            <p onClick={() => navigate("/MyPage")}>{userNickname} {userName}님 환영합니다.</p>
+          ) : (
+            <p onClick={() => navigate("/Login")}>로그인/회원가입</p>
+          )}
+        </S.Welcome>
         <S.Title>상명대 카풀 매칭 서비스<br />스뮤풀 SMUPOOL</S.Title>
         <S.Button>
-          <button>문의사항</button>
+          <button onClick={() => navigate("/Board")}>문의사항</button>
           <button onClick={handleChatbotToggle}>실시간 상담</button>
         </S.Button>
         {chatbotVisible && <Chatbot onClose={() => setChatbotVisible(false)} />}
